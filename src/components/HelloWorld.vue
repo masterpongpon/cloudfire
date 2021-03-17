2e862b02bcf8c69123a16bd0a8a7a8f34bd17d1c
masterpongpon <kira_songpon@hotmail.com>
2021-03-17 12:19:47 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,131 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <b-container>
+    <b-row align-h="center">
+      <b-card border-variant="dark" id="bg">
+        <b-row align-h="center">
+          <h1 class="mt-1 mr-2">Attraction</h1>
+        </b-row>
+        <b-row align-h="center">
+          <p class="mt-1 mr-2">Attraction Name :</p>
+          <b-form-input
+            id="inline-form-input-name"
+            class="col-5 mr-2"
+            v-model="name"
+          ></b-form-input>
+        </b-row>
+        <b-row align-h="center">
+          <p class="mt-1 mr-2">ความนิยม 3 เวลา(คน)</p>
+        </b-row>
+        <b-row align-h="center">
+          <p class="mr-2">เช้า</p>
+          <b-form-input
+            class="col-5"
+            v-model="morning"
+            type="range"
+            number
+            min="0"
+            max="100"
+            step="1"
+          ></b-form-input>
+          <p class="text-monospace ml-2">
+            {{ morning }}
+          </p>
+        </b-row>
+        <b-row align-h="center" class="mt-2">
+          <p class="mr-2">เที่ยง</p>
+          <b-form-input
+            class="col-5"
+            v-model="noon"
+            type="range"
+            number
+            min="0"
+            max="100"
+            step="1"
+          ></b-form-input>
+          <p class="text-monospace ml-2">
+            {{ noon }}
+          </p>
+        </b-row>
+        <b-row align-h="center" class="mt-2">
+          <p class="mr-2">เย็น</p>
+          <b-form-input
+            class="col-5"
+            v-model="evening"
+            type="range"
+            number
+            min="0"
+            max="100"
+            step="1"
+          ></b-form-input>
+          <p class="text-monospace ml-2">
+            {{ evening }}
+          </p>
+        </b-row>
+
+        <b-row align-h="center" class="mt-3">
+          <router-link to="/login">
+            <b-button class="mr-2" variant="dark" pill @click="addData"
+              >Add Attraction</b-button
+            >
+          </router-link>
+        </b-row>
+      </b-card>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import firebase from "firebase/app";
+import { db } from "@/main.js";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      name: "",
+      morning: 0,
+      noon: 0,
+      evening: 0,
+    };
+  },
+  methods: {
+    addData() {
+      alert("Complete");
+      // เก็บข้อมูล Form ใน collection attraction ( มี 1 document แต่จะ update ข้อมูลเรื่อย ๆ )
+      const data = {
+        Name: this.name,
+        Morning: this.morning,
+        Aftenoon: this.noon,
+        Evening: this.evening,
+      };
+      db.collection("attraction")
+        .doc("at")
+        .set(data)
+        .then(function () {
+          console.log("Document successfully written! -> attraction");
+        })
+        .catch(function (error) {
+          console.error("Error writing document: ", error);
+        });
+      // เก็บข้อมูล Input Text ใน collection MyText (มีหลาย document ข้อมูลจะเพิ่มขึ้นเรื่อย ๆ )
+      const dataText = {
+        txt: this.txt,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      };
+      db.collection("MyText")
+        .doc()
+        .set(dataText)
+        .then(function () {
+          console.log("Document successfully written! -> MyText");
+        })
+        .catch(function (error) {
+          console.error("Error writing document: ", error);
+        });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+#bg {
+  background-color: rgb(118, 240, 154);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
+// Initialize Cloud Firestore through Firebase
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXUPwo2ZJtXO7ISEML6CrBiv7phFwKv_A",
+    authDomain: "datap-c22a6.firebaseapp.com",
+    databaseURL: "https://datap-c22a6-default-rtdb.firebaseio.com",
+    projectId: "datap-c22a6",
+    storageBucket: "datap-c22a6.appspot.com",
+    messagingSenderId: "283503899388",
+    appId: "1:283503899388:web:3d76d4de356286dd7d1d79",
+    measurementId: "G-2VEQKGTKBM"
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+export const db = firebase.firestore()
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
